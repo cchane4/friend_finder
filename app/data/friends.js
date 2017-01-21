7620c8afebd6fcf78aecda1e8238c2794e254ae6
Chad Chaney <cchane4@Chads-MacBook-Air.local>
2017-01-21 09:42:38 -0600

    push before class

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+module.exports = [{
+	name:"Chad", 
+	photo:" ", 
+	survey: []
+}]; 
+
+//this goes to routes.js which goes to the front end via the path you set out
+
--- server.js
+++ server.js
@@ -2,15 +2,23 @@
 var express = require('express'); 
 var bodyParser  = require('body-parser'); 
 var path = require('path'); 
-var fs = require ('fs'); //???NOT SURE IF YOU NEED THIS ////
+
 ///SETTING UP EXPRESS //
 var app = express(); 
 var PORT = 3000; 
 //SET UP EXPRESS FOR PARSING//
 app.use(bodyParser.json()); 
-app.use(bodyParserbody.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text()); 
 app.use (bodyParser.json({ type:'application/vnd.api+json'}));
 //SERVER LISTENING FOR REQUESTS///
-app.listen(3000);
+app.use('/', express.static(path.join(__dirname,'./public')));
+// this is a function bc module.exports is a function 
+var apiFn = require('./routing/apiRoutes.js'); 
+apiFn(app); 
+var htmlFn = require('./routing/htmlRoutes.js'); 
+htmlFn(app); 
+app.listen(3000,function(){ 
+	console.log('listening on port'+ PORT); 
+});
 // CHECK TO SEE IF THIS WORKS ^^^^^^
