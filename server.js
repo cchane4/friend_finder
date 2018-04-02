6fddddf73b01bff577486e481a2482cc107b40a3
Chad Chaney <cchane4@gmail.com>
2018-04-01 19:06:40 -0500

    revisions: add gitignore/editing server

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 //DEPENDENCIES//
-var express = require('express'); 
-var bodyParser  = require('body-parser'); 
-var path = require('path'); 
+var express = require('express');
+var bodyParser  = require('body-parser');
+var path = require('path');
 
 ///SETTING UP EXPRESS //
-var app = express(); 
-var PORT = 3000; 
+var app = express();
+var PORT = process.env.PORT || 3000;
 //SET UP EXPRESS FOR PARSING//
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.text()); 
-app.use (bodyParser.json({ type:'application/vnd.api+json'}));
-//SERVER LISTENING FOR REQUESTS///
-app.use('/', express.static(path.join(__dirname,'./public')));
-// this is a function bc module.exports is a function 
-var apiFn = require('./routing/apiRoutes.js'); 
-apiFn(app); 
-var htmlFn = require('./routing/htmlRoutes.js'); 
-htmlFn(app); 
-app.listen(3000,function(){ 
-	console.log('listening on port'+ PORT); 
+var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(bodyParser.json({ type: 'application/*+json'}))
+app.use(bodyParser.raw ({ type: 'application/vnd.custom-type'}))
+app.use(bodyParser.text({ type: 'text/html'}))
+
+
+app.listen(PORT,function(){
+	console.log('listening on PORT: '+ PORT);
 });
-// CHECK TO SEE IF THIS WORKS ^^^^^^
