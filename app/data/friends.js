47b4c11474417bb6bdc66005bbbd7fb27cdbebba
Chad Chaney <cchane4@gmail.com>
2018-04-02 21:02:49 -0500

    adding data to app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,72 @@
-module.exports = [{
-	name:"Chad", 
-	photo:" ", 
-	survey: []
-}]; 
+var friends = [
+        {
+          name: "Chad",
+          photo: " ",
+          scores: [
+            "5",
+            "4",
+            "3",
+            "2",
+            "4",
+            "5",
+            "5",
+            "3",
+            "1",
+            "2"
+          ]
+        },
+        {
+          name: "Ana",
+          photo: " ",
+          scores: [
+            "4",
+            "4",
+            "2",
+            "2",
+            "4",
+            "1",
+            "5",
+            "4",
+            "1",
+            "2"
+          ]
+        },
+        {
+
+          name: "Derek",
+          photo: " ",
+          scores: [
+            "3",
+            "4",
+            "2",
+            "3",
+            "4",
+            "1",
+            "5",
+            "4",
+            "1",
+            "5"
+          ]
+        },
+        ame: "Ricky",
+          photo: " ",
+          scores: [
+            "5",
+            "4",
+            "3",
+            "2",
+            "4",
+            "5",
+            "5",
+            "3",
+            "1",
+            "2"
+          ]
+        }
+
+    ];
+
+
 
 //this goes to routes.js which goes to the front end via the path you set out
 
--- server.js
+++ server.js
@@ -11,10 +11,11 @@ var jsonParser = bodyParser.json()
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(bodyParser.json({ type: 'application/*+json'}))
-app.use(bodyParser.raw ({ type: 'application/vnd.custom-type'}))
-app.use(bodyParser.text({ type: 'text/html'}))
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+app.use(bodyParser.text({ type: 'text/html' }))
 
+require("./app/routing/html-routes.js")(app);
 
 app.listen(PORT,function(){
 	console.log('listening on PORT: '+ PORT);
