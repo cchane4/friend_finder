8dd11b0bbd9d8a1fc93904bb02d26c7b796438ce
Chad Chaney <cchane4@gmail.com>
2018-07-26 15:34:25 -0500

    color changes and proper static page reference on server

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 //dependencies
 var express = require('express');
-var app = express();
 var bodyParser  = require('body-parser');
 var path = require('path');
-
+var app = express();
 // use any available port if 3000 is not available
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json({ type: 'application/*+json' }))
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
-app.use(bodyParser.text({ type: 'text/html' }))
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(express.static(path.join(__dirname + '/app/public')));
+
+//app.use(bodyParser.text({ type: 'text/html' }))
+
 
 //requiring the 2 routes and passing in express
 require("./app/routing/api-routes.js")(app);
